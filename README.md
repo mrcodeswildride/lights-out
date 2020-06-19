fbfb54a308eeeb8ae1b9aa880002619d3b99420e
David Keller <mrcodeswildride@gmail.com>
2020-06-19 13:03:04 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # lights-out
 
-This is a Lights Out game written in HTML, CSS, and JavaScript.
+Lights Out game written in HTML, CSS, and JavaScript.
 
 Play this game at [lightsout.mrcodeswildride.com](https://lightsout.mrcodeswildride.com/).
 Learn how to make this game at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,74 +1,96 @@
-var rows = document.querySelectorAll(".row");
-var squares = document.querySelectorAll(".row div");
-var messageDisplay = document.getElementById("message");
+let rows = document.getElementsByClassName(`row`)
+let squares = document.getElementsByClassName(`square`)
+let messageParagraph = document.getElementById(`messageParagraph`)
 
-var gameInProgress = true;
+let gameOver = false
 
-for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", clickSquare);
+randomSetup()
+
+for (let square of squares) {
+  square.addEventListener(`click`, clickSquare)
 }
 
-randomSetup();
-
 function randomSetup() {
-    while (lightsOut()) {
-        for (var i = 0; i < 100; i++) {
-            var randomSquareIndex = Math.floor(Math.random() * squares.length);
-            selectSquare(squares[randomSquareIndex]);
-        }
-    }
+  for (let i = 0; i < 100; i++) {
+    let randomNumber = Math.floor(Math.random() * squares.length)
+    selectSquare(squares[randomNumber])
+  }
 }
 
 function clickSquare() {
-    if (gameInProgress) {
-        selectSquare(this);
+  if (!gameOver) {
+    selectSquare(this)
 
-        if (lightsOut()) {
-            gameInProgress = false;
-            messageDisplay.innerHTML = "Good job!";
-        }
+    if (isSolved()) {
+      messageParagraph.innerHTML = `Good job!`
+      gameOver = true
     }
+  }
 }
 
 function selectSquare(square) {
-    toggle(square);
-    toggleNeighbor(square, -1, 0);
-    toggleNeighbor(square, 0, -1);
-    toggleNeighbor(square, 1, 0);
-    toggleNeighbor(square, 0, 1);
+  square.classList.toggle(`on`)
+
+  let squareToLeft = getNeighbor(square, -1, 0)
+  let squareToRight = getNeighbor(square, 1, 0)
+  let squareAbove = getNeighbor(square, 0, -1)
+  let squareBelow = getNeighbor(square, 0, 1)
+
+  if (squareToLeft != null) {
+    squareToLeft.classList.toggle(`on`)
+  }
+
+  if (squareToRight != null) {
+    squareToRight.classList.toggle(`on`)
+  }
+
+  if (squareAbove != null) {
+    squareAbove.classList.toggle(`on`)
+  }
+
+  if (squareBelow != null) {
+    squareBelow.classList.toggle(`on`)
+  }
 }
 
-function toggle(square) {
-    if (square.classList.contains("on")) {
-        square.classList.remove("on");
+function getNeighbor(square, xDiff, yDiff) {
+  let row = square.parentElement // row of square
+  let y // y coordinate of square, set below
+  let x // x coordinate of square, set below
+
+  // loop through rows to determine y
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i] == row) {
+      y = i // found matching row, so set y
     }
-    else {
-        square.classList.add("on");
+  }
+
+  // loop through squares in row to determine x
+  for (let i = 0; i < row.children.length; i++) {
+    if (row.children[i] == square) {
+      x = i // found matching square, so set x
     }
+  }
+
+  // row of neighbor square
+  let neighborRow = rows[y + yDiff]
+
+  if (neighborRow == null) {
+    // row is beyond edge, so no neighbor square
+    return null
+  }
+  else {
+    // if x + xDiff is beyond edge, will be null
+    return neighborRow.children[x + xDiff]
+  }
 }
 
-function toggleNeighbor(square, xDiff, yDiff) {
-    var x = parseInt(square.getAttribute("index"), 10);
-    var y = parseInt(square.parentElement.getAttribute("index"), 10);
-
-    var row = rows[y + yDiff];
-    var neighbor = null;
-
-    if (row) {
-        neighbor = row.querySelector('div[index="' + (x + xDiff) + '"]');
+function isSolved() {
+  for (let square of squares) {
+    if (square.classList.contains(`on`)) {
+      return false
     }
+  }
 
-    if (neighbor) {
-        toggle(neighbor);
-    }
-}
-
-function lightsOut() {
-    for (var i = 0; i < squares.length; i++) {
-        if (squares[i].classList.contains("on")) {
-            return false;
-        }
-    }
-
-    return true;
+  return true
 }
